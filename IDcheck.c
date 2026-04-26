73c9069f45cffe0b9a1736e1e1b02b9c183a1f24
chance-irumva <irumvachancelline@gmail.com>
2026-04-26 11:56:56 +0200

    this is program for finding person at the entrance

---
+++ IDcheck.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    int ID;
+     
+    printf("Enter the ID number:");
+    scanf("%d",&ID);
+
+    if(ID==119178){
+        printf("He is the one");
+
+    }
+    else{
+        printf("wrong person");
+    }
+    return 0;
+}
